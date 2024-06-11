a4b6502f4de2baa54ca4650927d6b8c6b3b338cc
Yoshi Miyamoto <pocketgroovy@gmail.com>
2024-06-11 17:55:11 +0200

    unit test

--- src/test/java/com/pocketgroovy/rss/demo/RssFeedDemo/RssFeedDemoApplicationTests.java
+++ src/test/java/com/pocketgroovy/rss/demo/RssFeedDemo/RssFeedDemoApplicationTests.java
@@ -7,10 +7,12 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest
 class RssFeedDemoApplicationTests {
 
