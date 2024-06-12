1593c0b151b52809f0dfe18e426f14b9654bfc13
Yoshi Miyamoto <pocketgroovy@gmail.com>
2024-06-12 08:23:19 +0200

    remove unused

--- src/test/java/com/pocketgroovy/rss/demo/RssFeedDemo/RssFeedDemoApplicationTests.java
+++ src/test/java/com/pocketgroovy/rss/demo/RssFeedDemo/RssFeedDemoApplicationTests.java
@@ -3,7 +3,6 @@ package com.pocketgroovy.rss.demo.RssFeedDemo;
 import com.pocketgroovy.rss.demo.RssFeedDemo.controller.FeedController;
 import com.pocketgroovy.rss.demo.RssFeedDemo.service.FeedMessageService;
 import com.pocketgroovy.rss.demo.RssFeedDemo.service.FeedService;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
